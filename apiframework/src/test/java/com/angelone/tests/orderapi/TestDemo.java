2eb76a11b97ba124ca832ee10267339277db4d71
ANGELTRADE\vidya.bhushan <vidya.bhushan@angelbroking.com>
2023-02-24 00:12:12 +0530

    Changes for maven run

---
+++ apiframework/src/test/java/com/angelone/tests/orderapi/TestDemo.java
@@ -0,0 +1,11 @@
+package com.angelone.tests.orderapi;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+	@Test
+	public void testDemo() throws Exception {
+		System.out.println("Demo  Test");
+	}
+}
